4dda79dd7bbb53851e306ecfbd541d55bb2c64bc
Riddhi Tiwari <tiwaririddhi666@gmail.com>
2025-04-30 20:19:51 +0530

    add code for app

---
+++ main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main(){
+   runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+//This widget is the root of your application.
+@override
+Widget build(BuildContext context)  {
+    return MaterialApp(
+        title: 'LACS',
+        theme: ThemeData(
+           //This is the theme of your application.
+           //
+           //Try running your application
+
+        )
+    )
+}
+
+
+
+
+
+}
+
